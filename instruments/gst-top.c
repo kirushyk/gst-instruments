007e392899d5a47586a7ba6754d34120b8d9cee3
Kyrylo Polezhaiev <kirushyk@gmail.com>
2015-08-13 15:43:33 +0300

    gst-top: Add compile-time error for non-supported platforms, tweak codestyle

--- instruments/gst-top.c
+++ instruments/gst-top.c
@@ -12,17 +12,19 @@ main (int argc, char *argv[])
   g_setenv ("DYLD_INSERT_LIBRARIES", "libgstintercept.dylib", TRUE);
 #elif defined(G_OS_UNIX)
   g_setenv ("LD_PRELOAD", "libgstintercept.so", TRUE);
+#else
+# error GStreamer API calls interception is not supported on this platform
 #endif
   
   setenv ("GST_DEBUG_DUMP_INTERCEPT_PATH", "gst-top.intercept", 1);
   
   gint status = 0;
   GError *error = NULL;
-  g_spawn_sync (NULL, argv + 1, NULL, G_SPAWN_SEARCH_PATH , NULL, NULL, NULL, NULL, &status, &error);
+  g_spawn_sync (NULL, argv + 1, NULL, G_SPAWN_SEARCH_PATH, NULL, NULL, NULL, NULL, &status, &error);
   
   if (error)
   {
-    g_print("%s\n", error->message);
+    g_print ("%s\n", error->message);
   }
   else
   {
