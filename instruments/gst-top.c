962c404df16c8f69f0acbf76b7703abb2c2c84d9
Kyrylo Polezhaiev <kirushyk@gmail.com>
2015-08-20 03:46:24 +0300

    gst-top: Count upstack element CPU consumption

--- instruments/gst-top.c
+++ instruments/gst-top.c
@@ -21,7 +21,8 @@ main (int argc, char *argv[])
 # error GStreamer API calls interception is not supported on this platform
 #endif
   
-  setenv ("GST_INTERCEPT_OUTPUT_FILE", dump_filename, 1);
+  g_setenv ("GST_INTERCEPT_OUTPUT_FILE", dump_filename, TRUE);
+  // g_setenv ("GST_DEBUG", "task:DEBUG", TRUE);
   
   gint status = 0;
   GError *error = NULL;
@@ -39,23 +40,119 @@ main (int argc, char *argv[])
   return 0;
 }
 
+typedef struct GstTaskRecord
+{
+  gpointer identifier;
+  guint64 total_downstack_time;
+  
+  gboolean in_upstack;
+  guint64 enter_upstack_time;
+  guint64 exit_upstack_time;
+  
+  guint64 total_upstack_time;
+  GString *name;
+} GstTaskRecord;
+GHashTable *tasks = NULL;
+
+typedef struct GstElementRecord
+{
+  gboolean is_subtop;
+  gpointer identifier;
+  guint64 total_time;
+  GString *name;
+} GstElementRecord;
+GHashTable *elements = NULL;
+
+void
+for_each_task (gpointer key, gpointer value, gpointer user_data)
+{
+  GstTaskRecord *task = value;
+  g_print ("%s: %5.3f ms\n", task->name->str, task->total_upstack_time * 0.001);
+}
+
+void
+for_each_element (gpointer key, gpointer value, gpointer user_data)
+{
+  GstElementRecord *element = value;
+  g_print ("%s: %5.3f ms\n", element->name->str, element->total_time * 0.001);
+}
+
 void
 parse_output (const char *filename)
 {
+  tasks = g_hash_table_new (g_direct_hash, g_direct_equal);
+  elements = g_hash_table_new (g_direct_hash, g_direct_equal);
+  
   FILE *input = fopen (filename, "rt");
+  if (input == NULL)
+    return;
+  
   while (!feof (input))
   {
     gchar event_name[1000];
     if (fscanf (input, "%s", event_name) == 1)
     {
-      g_print ("%s: ", event_name);
+      // g_print ("%s: ", event_name);
       if (g_ascii_strcasecmp (event_name, "element-entered") == 0)
       {
         gpointer task_id;
+        gchar from_element_name[1000];
+        gpointer from_element_id;
         gchar element_name[1000];
         gpointer element_id;
-        if (fscanf (input, "%p %s %p\n", &task_id, element_name, &element_id) == 3)
-          g_print ("%p %s %p\n", task_id, element_name, element_id);
+        guint64 thread_time;
+        if (fscanf (input, "%p %s %p %s %p %" G_GUINT64_FORMAT"\n", &task_id, from_element_name, &from_element_id, element_name, &element_id, &thread_time) == 6)
+        {
+          GstElementRecord *element = g_hash_table_lookup (elements, element_id);
+          if (!element)
+          {
+            element = g_new0 (GstElementRecord, 1);
+            element->is_subtop = FALSE;
+            element->identifier = element_id;
+            element->total_time = 0;
+            element->name = g_string_new (element_name);
+            g_hash_table_insert (elements, element_id, element);
+          }
+          
+          g_assert_true(element != NULL);
+            
+          GstTaskRecord *task = g_hash_table_lookup (tasks, task_id);
+          if (!task)
+          {
+            task = g_new0 (GstTaskRecord, 1);
+            task->identifier = task_id;
+            task->total_upstack_time = 0;
+            task->enter_upstack_time = 0;
+            task->in_upstack = TRUE;
+            g_hash_table_insert (tasks, task_id, task);
+          }
+            
+          g_assert_true(task != NULL);
+            
+          if (task->in_upstack)
+          {
+            element->is_subtop = TRUE;
+            task->exit_upstack_time = thread_time;
+            task->total_upstack_time += task->exit_upstack_time - task->enter_upstack_time;
+            task->in_upstack = FALSE;
+            task->name = g_string_new (from_element_name);
+            if (task->name == NULL)
+            {
+            }
+            else
+            {
+              if (task->name->str[3] == '0')
+              {
+                fprintf(stderr, "\n%lu\n", thread_time);
+              }
+            }
+          }
+          else
+          {
+            element->is_subtop = FALSE;
+          }
+          task->total_downstack_time = 0;
+        }
         else
         {
           g_printerr ("couldn't parse event: %s\n", event_name);
@@ -67,12 +164,48 @@ parse_output (const char *filename)
         gpointer task_id;
         gchar element_name[1000];
         gpointer element_id;
+        guint64 thread_time;
         guint64 duration;
-        if (fscanf (input, "%p %s %p %" G_GUINT64_FORMAT"\n", &task_id, element_name, &element_id, &duration) == 4)
-          g_print ("%p %s %p %" G_GUINT64_FORMAT"\n", task_id, element_name, element_id, duration);
+        if (fscanf (input, "%p %s %p %" G_GUINT64_FORMAT" %" G_GUINT64_FORMAT"\n", &task_id, element_name, &element_id, &thread_time, &duration) == 5)
+        {
+          GstTaskRecord *task = g_hash_table_lookup (tasks, task_id);
+          if (!task)
+          {
+            g_printerr ("couldn't find task %p\n", task_id);
+            exit (2);
+          }
+          GstElementRecord *element = g_hash_table_lookup (elements, element_id);
+          if (!element)
+          {
+            g_printerr ("couldn't find element %p: %s\n", element_id, element_name);
+            exit (2);
+          }
+          element->total_time += duration - task->total_downstack_time;
+          task->total_downstack_time = duration;
+          if (element->is_subtop)
+          {
+            task->enter_upstack_time = thread_time;
+            task->in_upstack = TRUE;
+          }
+          else
+          {
+            task->in_upstack = FALSE;
+          }
+          
+          // g_print ("%p %s %p %" G_GUINT64_FORMAT"\n", task_id, element_name, element_id, duration);
+        }
         else
         {
           g_printerr ("couldn't parse event: %s\n", event_name);
+          exit (2);
+        }
+      }
+      else if (g_ascii_strcasecmp (event_name, "task") == 0)
+      {
+        gpointer task_id;
+        if (fscanf (input, "%p\n", &task_id) != 1)
+        {
+          g_printerr ("couldn't parse task\n");
           exit (2);
         }
       }
@@ -84,6 +217,13 @@ parse_output (const char *filename)
     }
   }
   fclose (input);
+  
+  g_hash_table_foreach (tasks, for_each_task, NULL);
+  g_print ("\n");
+  g_hash_table_foreach (elements, for_each_element, NULL);
+  
+  g_hash_table_destroy (elements);
+  g_hash_table_destroy (tasks);
 }
 
 
