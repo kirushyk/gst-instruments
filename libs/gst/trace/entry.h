ce919c2741ec407d9b250905937d854562d8514c
Kyrylo Polezhaiev <kirushyk@gmail.com>
2015-11-29 20:13:50 +0200

    trace: Add trace entry type enum

---
+++ libs/gst/trace/entry.h
@@ -0,0 +1,31 @@
+/* GStreamer Instruments
+ * Copyright (C) 2015 Kyrylo Polezhaiev <kirushyk@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify i t under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __GST_TRACE_ENTRY_H__
+#define __GST_TRACE_ENTRY_H__
+
+typedef enum GstTraceEntryType
+{
+  GST_TRACE_ENTRY_TYPE_ELEMENT_DISCOVERED,
+  GST_TRACE_ENTRY_TYPE_ELEMENT_ENTERED,
+  GST_TRACE_ENTRY_TYPE_ELEMENT_EXITED,
+  GST_TRACE_ENTRY_TYPE_DATA_SENT
+} GstTraceEntryType;
+
+#endif
