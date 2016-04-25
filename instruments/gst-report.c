f2ca4e54e0f6045fc675aea61d4ee4d9a429f40d
Kyrylo Polezhaiev <kirushyk@gmail.com>
2016-04-25 23:47:46 +0300

    gst-report: Temporary specify relative paths to headers

--- instruments/gst-report.c
+++ instruments/gst-report.c
@@ -19,9 +19,9 @@
 
 #include "formatters.h"
 #include <config.h>
-#include "gstgraveyard.h"
-#include "gstpadheadstone.h"
-#include "gstelementheadstone.h"
+#include "../libs/gst/trace/gstgraveyard.h"
+#include "../libs/gst/trace//gstpadheadstone.h"
+#include "../libs/gst/trace//gstelementheadstone.h"
 
 gdouble from = 0, till = 0;
 GstClockTime from_ns = GST_CLOCK_TIME_NONE, till_ns = GST_CLOCK_TIME_NONE;
