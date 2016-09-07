6eba5315c8e2da3faf557274790e9431d614f3ab
Kyrylo Polezhaiev <kirushyk@gmail.com>
2016-09-07 13:51:24 +0300

    build: Fix configuration errors handling

--- autogen.sh
+++ autogen.sh
@@ -1,18 +1,31 @@
 #!/bin/sh
 
+echo "Checking for autoreconf..."
 which "autoreconf" 2>/dev/null || {
   echo "Please install autoconf."
   exit 1
 }
 
+echo "Checking for pkg-config..."
 which "pkg-config" 2>/dev/null || {
   echo "Please install pkg-config."
+  exit 1
+}
+
+echo "Checking for aclocal..."
+which "aclocal" 2>/dev/null || {
+  echo "Please install automake."
   exit 1
 }
 
 package=gst-instruments
 
 autoreconf -vif
-./configure $*
+#./configure $*
+
+./configure $* || {
+  echo "Configure failed"
+  exit 1
+}
 
 echo "Now type 'make' to compile $package."
