41005ed5aa79bfe58f338b0177b8ffb290b607a0
Kyrylo Polezhaiev <kirushyk@gmail.com>
2016-10-05 17:32:37 +0300

    gst-report: Format code better

--- tools/gst-report.c
+++ tools/gst-report.c
@@ -25,20 +25,32 @@
 
 gdouble from = 0, till = 0;
 GstClockTime from_ns = GST_CLOCK_TIME_NONE, till_ns = GST_CLOCK_TIME_NONE;
-gboolean show_memory = FALSE, show_types = FALSE, hierarchy = FALSE, nested_time = FALSE, dot = FALSE, simple_pads = FALSE, dur_only = FALSE, mu = FALSE;
+gboolean show_memory = FALSE, show_types = FALSE, hierarchy = FALSE,
+  nested_time = FALSE, dot = FALSE, simple_pads = FALSE, dur_only = FALSE,
+  mu = FALSE;
 
 static GOptionEntry entries[] =
 {
-  { "from",      0, 0, G_OPTION_ARG_DOUBLE, &from,        "Do not take events before timestamp into account", NULL },
-  { "till",      0, 0, G_OPTION_ARG_DOUBLE, &till,        "Do not take events after timestamp into account",  NULL },
-  { "memory",    0, 0, G_OPTION_ARG_NONE,   &show_memory, "Show memory usage",                                NULL },
-  { "types",     0, 0, G_OPTION_ARG_NONE,   &show_types,  "Show types of elements",                           NULL },
-  { "hierarchy", 0, 0, G_OPTION_ARG_NONE,   &hierarchy,   "Show hierarchy of elements",                       NULL },
-  { "textpads",  0, 0, G_OPTION_ARG_NONE,   &simple_pads, "Show simple pad nodes (without SVG inclusion)",    NULL },
-  { "nested",    0, 0, G_OPTION_ARG_NONE,   &nested_time, "Include CPU time spent by nested elements",        NULL },
-  { "dot",       0, 0, G_OPTION_ARG_NONE,   &dot,         "Output in DOT format",                             NULL },
-  { "mu",        0, 0, G_OPTION_ARG_NONE,   &mu,          "Use μ (mu) symbol instead of u as micro prefix",   NULL },
-  { "duration",  0, 0, G_OPTION_ARG_NONE,   &dur_only,    "Only show duration",                               NULL },
+  { "from",      0, 0, G_OPTION_ARG_DOUBLE, &from,
+    "Do not take events before timestamp into account", NULL },
+  { "till",      0, 0, G_OPTION_ARG_DOUBLE, &till,
+    "Do not take events after timestamp into account",  NULL },
+  { "memory",    0, 0, G_OPTION_ARG_NONE,   &show_memory,
+    "Show memory usage",                                NULL },
+  { "types",     0, 0, G_OPTION_ARG_NONE,   &show_types,
+    "Show types of elements",                           NULL },
+  { "hierarchy", 0, 0, G_OPTION_ARG_NONE,   &hierarchy,
+    "Show hierarchy of elements",                       NULL },
+  { "textpads",  0, 0, G_OPTION_ARG_NONE,   &simple_pads,
+    "Show simple pad nodes (without SVG inclusion)",    NULL },
+  { "nested",    0, 0, G_OPTION_ARG_NONE,   &nested_time,
+    "Include CPU time spent by nested elements",        NULL },
+  { "dot",       0, 0, G_OPTION_ARG_NONE,   &dot,
+    "Output in DOT format",                             NULL },
+  { "mu",        0, 0, G_OPTION_ARG_NONE,   &mu,
+    "Use μ (mu) symbol instead of u as micro prefix",   NULL },
+  { "duration",  0, 0, G_OPTION_ARG_NONE,   &dur_only,
+    "Only show duration",                               NULL },
   { NULL }
 };
 
@@ -58,7 +70,8 @@ render_pad (gpointer key, gpointer value, gpointer user_data)
   GstElementHeadstone *element = (GstElementHeadstone *)user_data;
   gint space = (element->nesting_level + 1);
   render_space (space);
-  g_print ("subgraph cluster_eg%p_pad_%p {\n", element->identifier, pad->identifier);
+  g_print ("subgraph cluster_eg%p_pad_%p {\n", element->identifier,
+    pad->identifier);
   space++;
   render_space (space);
   g_print ("style=\"invis\";\n");
@@ -66,9 +79,13 @@ render_pad (gpointer key, gpointer value, gpointer user_data)
   g_print ("label=\"\";\n");
   render_space (space);
   if (simple_pads) {
-    g_print ("en%p_pad_%p [shape=\"box\" label=\"%s\", fillcolor=\"#ffffff\"];\n", element->identifier, pad->identifier, pad->mode == GST_PAD_MODE_PULL ? "[]=>" : "=>[]");
+    g_print ("en%p_pad_%p [shape=\"box\" label=\"%s\", " \
+      "fillcolor=\"#ffffff\"];\n", element->identifier, pad->identifier,
+      pad->mode == GST_PAD_MODE_PULL ? "[]=>" : "=>[]");
   } else {
-    g_print ("en%p_pad_%p [shape=\"none\", label=\"\", image=\"" DATADIR "/" PACKAGE_NAME "/%s.svg\", fillcolor=\"#ffffff\"];\n", element->identifier, pad->identifier, pad->mode == GST_PAD_MODE_PULL ? "pull" : "push");
+    g_print ("en%p_pad_%p [shape=\"none\", label=\"\", image=\"" DATADIR "/" \
+      PACKAGE_NAME "/%s.svg\", fillcolor=\"#ffffff\"];\n", element->identifier,
+      pad->identifier, pad->mode == GST_PAD_MODE_PULL ? "pull" : "push");
   }
   space--;
   render_space (space);
@@ -81,7 +98,9 @@ bind_src_pad (gpointer key, gpointer value, gpointer user_data)
   GstPadHeadstone *pad = (GstPadHeadstone *)value;
   if (pad->direction == GST_PAD_SRC) {
     GstPadHeadstone *sink_pad = (GstPadHeadstone *)user_data;
-    g_print ("en%p_pad_%p -> en%p_pad_%p [style=\"invis\"];\n", sink_pad->parent_element, sink_pad->identifier, pad->parent_element, pad->identifier);
+    g_print ("en%p_pad_%p -> en%p_pad_%p [style=\"invis\"];\n",
+      sink_pad->parent_element, sink_pad->identifier, pad->parent_element,
+      pad->identifier);
   }
 }
 
@@ -96,11 +115,13 @@ bind_sink_pad (gpointer key, gpointer value, gpointer user_data)
 }
 
 void
-render_headstone (GstGraveyard *graveyard, GstElementHeadstone *element, gsize max_length, gsize max_type_name_length)
+render_headstone (GstGraveyard *graveyard, GstElementHeadstone *element,
+  gsize max_length, gsize max_type_name_length)
 {
   gint j;
   
-  guint64 total_cpu_time = nested_time ? gst_element_headstone_get_nested_time (element) : element->total_cpu_time;
+  guint64 total_cpu_time = nested_time ?
+    gst_element_headstone_get_nested_time (element) : element->total_cpu_time;
   gsize space = element->nesting_level;
   
   if (hierarchy) {
@@ -108,7 +129,8 @@ render_headstone (GstGraveyard *graveyard, GstElementHeadstone *element, gsize m
   }
   
   if (dot) {
-    g_print ("%sgraph cluster_eg%p {\n", element->parent == NULL ? "di" : "sub", element->identifier);
+    g_print ("%sgraph cluster_eg%p {\n", element->parent == NULL ? "di" : "sub",
+      element->identifier);
   }
   
   if (dot && (element->parent == NULL)) {
@@ -135,13 +157,16 @@ render_headstone (GstGraveyard *graveyard, GstElementHeadstone *element, gsize m
     g_print ("label=\"pipeline0\";\n");
     render_space (space);
 #ifdef __APPLE__
-    g_print ("node [style=\"filled\", shape=box, fontsize=\"14\", fontname=\"Avenir Next\", margin=\"0.1,0.1\"];\n");
+    g_print ("node [style=\"filled\", shape=box, fontsize=\"14\", "
+             "fontname=\"Avenir Next\", margin=\"0.1,0.1\"];\n");
 #else
-    g_print ("node [style=\"filled\", shape=box, fontsize=\"14\", margin=\"0.1,0.1\"];\n");
+    g_print ("node [style=\"filled\", shape=box, fontsize=\"14\", "
+             "margin=\"0.1,0.1\"];\n");
 #endif
     render_space (space);
 #ifdef __APPLE__
-    g_print ("edge [labelfontsize=\"14\", fontsize=\"14\", fontname=\"Avenir Next\"];\n");
+    g_print ("edge [labelfontsize=\"14\", fontsize=\"14\", "
+             "fontname=\"Avenir Next\"];\n");
 #else
     g_print ("edge [labelfontsize=\"14\", fontsize=\"14\"];\n");
 #endif
@@ -154,10 +179,12 @@ render_headstone (GstGraveyard *graveyard, GstElementHeadstone *element, gsize m
     g_hash_table_foreach (element->pads, render_pad, element);
     g_hash_table_foreach (element->pads, bind_sink_pad, element);
     render_space (space);
-    guint64 total_cpu_time = nested_time ? gst_element_headstone_get_nested_time (element) : element->total_cpu_time;
+    guint64 total_cpu_time = nested_time ?
+      gst_element_headstone_get_nested_time (element) : element->total_cpu_time;
     gchar *time_string = format_time (total_cpu_time, mu);
     
-    g_print ("label=<<TABLE BORDER=\"0\" CELLBORDER=\"0\" CELLSPACING=\"0\" CELLPADDING=\"0\">"
+    g_print ("label=<<TABLE BORDER=\"0\" CELLBORDER=\"0\" CELLSPACING=\"0\" "
+             "CELLPADDING=\"0\">"
              "<TR>"
              "<TD COLSPAN=\"2\" ALIGN=\"RIGHT\">%s</TD>"
              "</TR>"
@@ -176,14 +203,18 @@ render_headstone (GstGraveyard *graveyard, GstElementHeadstone *element, gsize m
              "<TD ALIGN=\"RIGHT\">CPU:</TD>"
              "<TD ALIGN=\"RIGHT\">%5.1f%%</TD>"
              "</TR>"
-             "</TABLE>>;\n", element->type_name->str, element->name->str, time_string, total_cpu_time * 100.f / graveyard->total_cpu_time, (nested_time ? gst_element_headstone_get_nested_load (element) : element->cpu_load) * 100.0f);
-  
+             "</TABLE>>;\n", element->type_name->str, element->name->str,
+      time_string, total_cpu_time * 100.f / graveyard->total_cpu_time,
+      (nested_time ? gst_element_headstone_get_nested_load (element) :
+        element->cpu_load) * 100.0f);
+
     g_free (time_string);
   } else {
     g_print ("%s", element->name->str);
   }
 
-  space = max_length - element->name->len - ((hierarchy && !dot) ? element->nesting_level : 0);
+  space = max_length - element->name->len -
+    ((hierarchy && !dot) ? element->nesting_level : 0);
   for (j = 0; j < space; j++) {
     g_print (" ");
   }
@@ -200,15 +231,19 @@ render_headstone (GstGraveyard *graveyard, GstElementHeadstone *element, gsize m
       g_print (" ");
     }
   }
-  
+
   if (!dot) {
     gchar *time_string = format_time (total_cpu_time, mu);
-    g_print (" %5.1f  %5.1f  %8s", (nested_time ? gst_element_headstone_get_nested_load (element) : element->cpu_load) * 100.f, total_cpu_time * 100.0f / graveyard->total_cpu_time, time_string);
+    g_print (" %5.1f  %5.1f  %8s",
+      (nested_time ? gst_element_headstone_get_nested_load (element) :
+        element->cpu_load) * 100.f,
+      total_cpu_time * 100.0f / graveyard->total_cpu_time, time_string);
     g_free (time_string);
   }
-  
+
   if (!dot && show_memory) {
-    gchar *memory_received_size_string = format_memory_size (element->bytes_received);
+    gchar *memory_received_size_string =
+      format_memory_size (element->bytes_received);
     gchar *memory_sent_size_string = format_memory_size (element->bytes_sent);
     g_print (" %9s %9s", memory_received_size_string, memory_sent_size_string);
     g_free (memory_sent_size_string);
@@ -226,21 +261,26 @@ render_connection (gpointer key, gpointer value, gpointer user_data)
     gint space = (element->nesting_level + 1);
     render_space (space);
     gchar *memory_sent_size_string = format_memory_size (pad->bytes);
-    g_print ("en%p_pad_%p -> en%p_pad_%p [label=\"%s\"];\n", element->identifier, pad->identifier, pad->peer_element, pad->peer, memory_sent_size_string);
+    g_print ("en%p_pad_%p -> en%p_pad_%p [label=\"%s\"];\n",
+      element->identifier, pad->identifier, pad->peer_element, pad->peer,
+      memory_sent_size_string);
     g_free (memory_sent_size_string);
   }
 }
 
 void
-render_container (GstGraveyard *graveyard, GstElementHeadstone *element, gsize max_length, gsize max_type_name_length)
+render_container (GstGraveyard *graveyard, GstElementHeadstone *element,
+  gsize max_length, gsize max_type_name_length)
 {
   GList *child;
   render_headstone (graveyard, element, max_length, max_type_name_length);
   for (child = element->children; child != NULL; child = child->next) {
     GstElementHeadstone *child_element = child->data;
-    render_container (graveyard, child_element, max_length, max_type_name_length);
+    render_container (graveyard, child_element, max_length,
+      max_type_name_length);
     if (dot) {
-      g_hash_table_foreach (child_element->pads, render_connection, child_element);
+      g_hash_table_foreach (child_element->pads, render_connection,
+        child_element);
     }
   }
   if (dot) {
@@ -256,10 +296,10 @@ main (gint argc, gchar *argv[])
   gsize max_length = 0, max_type_name_length = 0;
   GError *error = NULL;
   GOptionContext *option_context;
-  
+
   g_set_prgname ("gst-report-" GST_API_VERSION);
   g_set_application_name ("GStreamer Report Tool");
-  
+
   option_context = g_option_context_new (NULL);
   g_option_context_add_main_entries (option_context, entries, NULL);
   g_option_context_set_summary (option_context, g_get_application_name ());
@@ -269,21 +309,22 @@ main (gint argc, gchar *argv[])
     return 1;
   }
   g_option_context_free (option_context);
-  
+
   if (from > 0) {
     from_ns = from * GST_SECOND;
   }
   if (till > 0) {
     till_ns = till * GST_SECOND;
   }
-  
+
   if (dot) {
     hierarchy = TRUE;
     show_types = TRUE;
     show_memory = TRUE;
   }
   
-  GstGraveyard *graveyard = gst_graveyard_new_from_trace (argv[argc - 1], from_ns, till_ns, dur_only);
+  GstGraveyard *graveyard = gst_graveyard_new_from_trace (argv[argc - 1],
+    from_ns, till_ns, dur_only);
   if (graveyard == NULL) {
     g_print ("could not read trace file: %s\n", argv[argc - 1]);
     return 3;
@@ -293,9 +334,10 @@ main (gint argc, gchar *argv[])
     g_print ("%" G_GUINT64_FORMAT, graveyard->duration);
     return 0;
   }
-  
+
   for (i = 0; i < graveyard->elements_sorted->len; i++) {
-    GstElementHeadstone *element = g_array_index (graveyard->elements_sorted, GstElementHeadstone *, i);
+    GstElementHeadstone *element = g_array_index (graveyard->elements_sorted,
+      GstElementHeadstone *, i);
     if (element->name->len + element->nesting_level > max_length) {
       max_length = element->name->len + element->nesting_level;
     }
@@ -303,7 +345,7 @@ main (gint argc, gchar *argv[])
       max_type_name_length = element->type_name->len;
     }
   }
-  
+
   if (!dot) {
     g_print ("ELEMENT");
     gsize space = (max_length > 7) ? max_length - 7 : 0;
@@ -330,7 +372,8 @@ main (gint argc, gchar *argv[])
   }
   
   for (i = 0; i < graveyard->elements_sorted->len; i++) {
-    GstElementHeadstone *element = g_array_index (graveyard->elements_sorted, GstElementHeadstone *, i);
+    GstElementHeadstone *element = g_array_index (graveyard->elements_sorted,
+      GstElementHeadstone *, i);
     if (hierarchy) {
       if (element->parent == NULL) {
         render_container (graveyard, element, max_length, max_type_name_length);
