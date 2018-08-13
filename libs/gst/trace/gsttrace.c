9f26032aec9d00d0b5ba2e8c9647e3185f768b1a
sewon.oh <sewon.oh@samsung.com>
2018-08-13 14:41:49 +0900

    Edit typo in comments
    It is trivial.

--- libs/gst/trace/gsttrace.c
+++ libs/gst/trace/gsttrace.c
@@ -2,7 +2,7 @@
  * Copyright (C) 2015 Kyrylo Polezhaiev <kirushyk@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
- * modify i t under the terms of the GNU Library General Public
+ * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
  *
