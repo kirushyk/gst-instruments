493d871687a396126cea35bf3fcfc33e458fef90
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-07-15 06:50:22 +0300

    ui/macos: Add dummy app

---
+++ ui/macos/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  macos
+//
+//  Created by Svetlana on 08.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
